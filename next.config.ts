1dbd75fcabbd34750a027868bdae04b38f69a45c
TheMarkest <markbogdanovofficial@gmail.com>
2026-01-25 16:04:37 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // Важно: раскомментируйте строку ниже, если сайт опубликован по адресу themarkest.github.io/themarkest.me/
+  // Если у вас подключен свой домен (themarkest.me), эту строку нужно закомментировать или удалить.
+  basePath: '/themarkest.me',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
